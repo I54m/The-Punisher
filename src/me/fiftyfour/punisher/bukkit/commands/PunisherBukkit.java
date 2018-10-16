1a24a3e777a5cf24cc6b97a18ce7fb1bd5cd0dd0
BuildTools <unconfigured@null.spigotmc.org>
2018-10-16 16:34:15 +1300

    Added a version command for bukkit and bungeecord

---
+++ src/me/fiftyfour/punisher/bukkit/commands/PunisherBukkit.java
@@ -0,0 +1,37 @@
+package me.fiftyfour.punisher.bukkit.commands;
+
+import me.fiftyfour.punisher.bukkit.BukkitMain;
+import me.fiftyfour.punisher.systems.UpdateChecker;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+
+public class PunisherBukkit implements CommandExecutor {
+    private String prefix = ChatColor.GRAY + "[" + ChatColor.RED + "Punisher" + ChatColor.GRAY + "] " + ChatColor.RESET;
+    private BukkitMain plugin = BukkitMain.getInstance();
+
+    @Override
+    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+        if (commandSender.hasPermission("Punisher.bukkit.admin")) {
+            if (strings[0].equalsIgnoreCase("version")) {
+                if (!BukkitMain.update) {
+                    commandSender.sendMessage(prefix + ChatColor.GREEN + "Current Bukkit Version: " + plugin.getDescription().getVersion());
+                    commandSender.sendMessage(prefix + ChatColor.GREEN + "This is the latest version!");
+                } else {
+                    commandSender.sendMessage(prefix + ChatColor.RED + "Current Bukkit Version: " + plugin.getDescription().getVersion());
+                    commandSender.sendMessage(prefix + ChatColor.RED + "Latest version: " + UpdateChecker.getCurrentVersion());
+                }
+                return true;
+            } else {
+                commandSender.sendMessage(prefix + ChatColor.RED + "Check the current bukkit version");
+                commandSender.sendMessage(prefix + ChatColor.RED + "Usage: /punisherbukkit version");
+            }
+            return false;
+        }else{
+            commandSender.sendMessage(ChatColor.RED + "You do not have permission to use this command!");
+            return false;
+        }
+    }
+}
