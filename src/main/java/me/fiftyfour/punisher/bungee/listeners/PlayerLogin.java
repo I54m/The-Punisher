ae21d7397f53ff4c2bd3da11bcba59713f6d4b23
54mpenguin <54mpenguin@gmail.com>
2019-09-08 17:15:01 +1200

    Optimized async ip updates and Implemented Custom Exceptions

--- src/main/java/me/fiftyfour/punisher/bungee/listeners/PlayerLogin.java
+++ src/main/java/me/fiftyfour/punisher/bungee/listeners/PlayerLogin.java
@@ -2,10 +2,11 @@ package me.fiftyfour.punisher.bungee.listeners;
 
 import me.fiftyfour.punisher.bungee.BungeeMain;
 import me.fiftyfour.punisher.bungee.chats.StaffChat;
-import me.fiftyfour.punisher.universal.exceptions.DataFecthException;
 import me.fiftyfour.punisher.bungee.handlers.ErrorHandler;
 import me.fiftyfour.punisher.bungee.managers.PunishmentManager;
 import me.fiftyfour.punisher.bungee.objects.Punishment;
+import me.fiftyfour.punisher.universal.exceptions.DataFecthException;
+import me.fiftyfour.punisher.universal.exceptions.PunishmentsDatabaseException;
 import me.fiftyfour.punisher.universal.fetchers.NameFetcher;
 import me.fiftyfour.punisher.universal.fetchers.UserFetcher;
 import me.lucko.luckperms.LuckPerms;
@@ -37,11 +38,9 @@ public class PlayerLogin implements Listener {
 
     @EventHandler(priority = EventPriority.HIGHEST)
     public void onLogin(LoginEvent event) {
-        event.registerIntent(plugin);
         UUID uuid = event.getConnection().getUniqueId();
         String fetcheduuid = uuid.toString().replace("-", "");
         PendingConnection connection = event.getConnection();
-        ArrayList<String> altslist = new ArrayList<>();
         String targetName = NameFetcher.getName(fetcheduuid);
         try {
             User user = LuckPerms.getApi().getUser(uuid);
@@ -55,7 +54,7 @@ public class PlayerLogin implements Listener {
                 } catch (Exception e) {
                     try {
                         throw new DataFecthException("User instance required for ban bypass check", targetName, "User Instance", PlayerLogin.class.getName(), e);
-                    }catch (DataFecthException dfe){
+                    } catch (DataFecthException dfe) {
                         ErrorHandler errorHandler = ErrorHandler.getInstance();
                         errorHandler.log(dfe);
                         errorHandler.loginError(event);
@@ -68,7 +67,6 @@ public class PlayerLogin implements Listener {
             if (user == null) {
                 event.setCancelled(true);
                 event.setCancelReason(new TextComponent(ChatColor.RED + "ERROR: Luckperms was unable to fetch your permission data!\n If this error persists please contact an admin+"));
-                event.completeIntent(plugin);
                 return;
             }
             ContextManager cm = LuckPerms.getApi().getContextManager();
@@ -106,109 +104,126 @@ public class PlayerLogin implements Listener {
                             event.setCancelled(true);
                             event.setCancelReason(new TextComponent(ChatColor.translateAlternateColorCodes('&', banMessage)));
                         }
-                        event.completeIntent(plugin);
                         return;
                     }
                 }
             }
-            event.completeIntent(plugin);
-            plugin.getProxy().getScheduler().runAsync(plugin, () -> {
-                plugin.getProxy().getScheduler().runAsync(plugin, () -> {
-                    try {
-                        //update ip and make sure there is one in the database
-                        String sqlip = "SELECT * FROM `altlist` WHERE UUID='" + fetcheduuid + "'";
-                        PreparedStatement stmtip = plugin.connection.prepareStatement(sqlip);
-                        ResultSet resultsip = stmtip.executeQuery();
-                        if (resultsip.next()) {
-                            if (!resultsip.getString("ip").equals(connection.getAddress().getHostString())) {
-                                String oldip = resultsip.getString("ip");
-                                String sqlipadd = "UPDATE `altlist` SET `ip`='" + connection.getAddress().getHostString() + "' WHERE `ip`='" + oldip + "' ;";
-                                PreparedStatement stmtipadd = plugin.connection.prepareStatement(sqlipadd);
-                                stmtipadd.executeUpdate();
-                                stmtipadd.close();
-                            }
-                        } else {
-                            String sql1 = "INSERT INTO `altlist` (`UUID`, `ip`) VALUES ('" + fetcheduuid + "', '" + connection.getAddress().getHostString() + "');";
-                            PreparedStatement stmt1 = plugin.connection.prepareStatement(sql1);
-                            stmt1.executeUpdate();
-                            stmt1.close();
-                        }
-                        stmtip.close();
-                        resultsip.close();
-                    } catch (SQLException sqle) {
-                        sqlException(sqle, event, targetName);
-                    }
-                });
-                plugin.getProxy().getScheduler().runAsync(plugin, () -> {
-                    try {
-                        //update ip hist
-                        String sql = "SELECT * FROM `iphist` WHERE UUID='" + fetcheduuid + "'";
-                        PreparedStatement stmt = plugin.connection.prepareStatement(sql);
-                        ResultSet results = stmt.executeQuery();
-                        if (results.next()) {
-                            if (!results.getString("ip").equals(connection.getAddress().getHostString())) {
-                                String addip = "INSERT INTO `iphist` (`UUID`, `date`, `ip`) VALUES ('" + fetcheduuid + "', '" + System.currentTimeMillis() + "', '" + connection.getAddress().getHostString() + "');";
-                                PreparedStatement addipstmt = plugin.connection.prepareStatement(addip);
-                                addipstmt.executeUpdate();
-                                addipstmt.close();
-                            }
-                        } else {
-                            String addip = "INSERT INTO `iphist` (`UUID`, `date`, `ip`) VALUES ('" + fetcheduuid + "', '" + System.currentTimeMillis() + "', '" + connection.getAddress().getHostString() + "');";
-                            PreparedStatement addipstmt = plugin.connection.prepareStatement(addip);
-                            addipstmt.executeUpdate();
-                            addipstmt.close();
-                        }
-                    } catch (SQLException sqle) {
-                        sqlException(sqle, event, targetName);
-                    }
-                });
-                if (!BungeeMain.RepStorage.contains(fetcheduuid)) {
-                    BungeeMain.RepStorage.set(fetcheduuid, 5.0);
-                }
-                try {
-                    //check for banned alts
-                    String ip = connection.getAddress().getHostString();
-                    String sqlip1 = "SELECT * FROM `altlist` WHERE ip='" + ip + "'";
-                    PreparedStatement stmtip1 = plugin.connection.prepareStatement(sqlip1);
-                    ResultSet resultsip1 = stmtip1.executeQuery();
-                    while (resultsip1.next()) {
-                        String concacc = resultsip1.getString("uuid");
-                        altslist.add(concacc);
-                    }
-                    stmtip1.close();
-                    resultsip1.close();
-                    altslist.remove(uuid.toString().replace("-", ""));
-                    if (!altslist.isEmpty()) {
-                        ArrayList<String> bannedalts = new ArrayList<>();
-                        for (String alts : altslist) {
-                            if (punishmngr.isBanned(alts)) {
-                                bannedalts.add(NameFetcher.getName(alts));
-                            }
-                        }
-                        if (!bannedalts.isEmpty()) {
-                            plugin.getProxy().getScheduler().schedule(plugin, () -> StaffChat.sendMessage(targetName + " Might have banned alts: " + bannedalts.toString().replace("[", "").replace("]", "")), 5, TimeUnit.SECONDS);
-                        }
-                    }
-                } catch (SQLException sqle) {
-                    sqlException(sqle, event, targetName);
-                }
-            });
         } catch (SQLException e) {
-            sqlException(e, event, targetName);
-            event.completeIntent(plugin);
+            try {
+                throw new PunishmentsDatabaseException("Revoking punishment", targetName, this.getClass().getName(), e);
+            } catch (PunishmentsDatabaseException pde) {
+                ErrorHandler errorHandler = ErrorHandler.getInstance();
+                errorHandler.log(pde);
+                errorHandler.loginError(event);
+            }
         }
-    }
+        //todo improve async stuff, might be improved now?
+        plugin.getProxy().getScheduler().runAsync(plugin, () -> {
+            try {
+                //update ip and make sure there is one in the database
+                String sqlip = "SELECT * FROM `altlist` WHERE UUID='" + fetcheduuid + "'";
+                PreparedStatement stmtip = plugin.connection.prepareStatement(sqlip);
+                ResultSet resultsip = stmtip.executeQuery();
+                if (resultsip.next()) {
+                    if (!resultsip.getString("ip").equals(connection.getAddress().getHostString())) {
+                        String oldip = resultsip.getString("ip");
+                        String sqlipadd = "UPDATE `altlist` SET `ip`='" + connection.getAddress().getAddress().getHostAddress() + "' WHERE `ip`='" + oldip + "' ;";
+                        PreparedStatement stmtipadd = plugin.connection.prepareStatement(sqlipadd);
+                        stmtipadd.executeUpdate();
+                        stmtipadd.close();
+                    }
+                } else {
+                    String sql1 = "INSERT INTO `altlist` (`UUID`, `ip`) VALUES ('" + fetcheduuid + "', '" + connection.getAddress().getAddress().getHostAddress() + "');";
+                    PreparedStatement stmt1 = plugin.connection.prepareStatement(sql1);
+                    stmt1.executeUpdate();
+                    stmt1.close();
+                }
+                stmtip.close();
+                resultsip.close();
+            } catch (SQLException sqle) {
+                try {
+                    throw new PunishmentsDatabaseException("Updating ip in altlist", targetName, PlayerLogin.class.getName(), sqle);
+                } catch (PunishmentsDatabaseException pde) {
+                    ErrorHandler errorHandler = ErrorHandler.getInstance();
+                    errorHandler.log(pde);
+                    errorHandler.loginError(event);
+                }
+            }
 
-    private void sqlException(SQLException e, LoginEvent event, String targetName) {
-        plugin.getLogger().severe(plugin.prefix + e);
-        plugin.getLogger().severe(plugin.prefix + PlayerLogin.class.getName() + " has thrown an exception while processing login for: " + targetName + "!");
-        plugin.getLogger().severe(plugin.prefix + "kicking player and returning to prevent further damage to database!");
-        BungeeMain.Logs.severe(PlayerLogin.class.getName() + " has thrown an exception while processing login for: " + targetName + "!");
-        BungeeMain.Logs.severe("kicking player and returning to prevent further damage to database!");
-        event.getConnection().disconnect(new TextComponent(plugin.prefix + "\n" + ChatColor.RED + "We encountered an error while processing your login!" +
-                "\nTo prevent further damage to our database we have kicked you!" +
-                "\nPlease Contact an Admin+ ASAP!" +
-                "\nIf you are an Admin+ please check that the database connection is functional" +
-                "\nand that there are no errors in the database itself or config.yml!"));
+            try {
+                //update ip hist
+                String sql = "SELECT * FROM `iphist` WHERE UUID='" + fetcheduuid + "'";
+                PreparedStatement stmt = plugin.connection.prepareStatement(sql);
+                ResultSet results = stmt.executeQuery();
+                if (results.next()) {
+                    if (!results.getString("ip").equals(connection.getAddress().getHostString())) {
+                        String addip = "INSERT INTO `iphist` (`UUID`, `date`, `ip`) VALUES ('" + fetcheduuid + "', '" + System.currentTimeMillis() + "', '" + connection.getAddress().getAddress().getHostAddress() + "');";
+                        PreparedStatement addipstmt = plugin.connection.prepareStatement(addip);
+                        addipstmt.executeUpdate();
+                        addipstmt.close();
+                    }
+                } else {
+                    String addip = "INSERT INTO `iphist` (`UUID`, `date`, `ip`) VALUES ('" + fetcheduuid + "', '" + System.currentTimeMillis() + "', '" + connection.getAddress().getAddress().getHostAddress() + "');";
+                    PreparedStatement addipstmt = plugin.connection.prepareStatement(addip);
+                    addipstmt.executeUpdate();
+                    addipstmt.close();
+                }
+            } catch (SQLException sqle) {
+                try {
+                    throw new PunishmentsDatabaseException("Updating ip in iphist", targetName, PlayerLogin.class.getName(), sqle);
+                } catch (PunishmentsDatabaseException pde) {
+                    ErrorHandler errorHandler = ErrorHandler.getInstance();
+                    errorHandler.log(pde);
+                    errorHandler.loginError(event);
+                }
+            }
+
+            if (!BungeeMain.RepStorage.contains(fetcheduuid)) {
+                BungeeMain.RepStorage.set(fetcheduuid, 5.0);
+            }
+
+            try {
+                //check for banned alts
+                ArrayList<String> altslist = new ArrayList<>();
+                String ip = connection.getAddress().getHostString();
+                String sqlip1 = "SELECT * FROM `altlist` WHERE ip='" + ip + "'";
+                PreparedStatement stmtip1 = plugin.connection.prepareStatement(sqlip1);
+                ResultSet resultsip1 = stmtip1.executeQuery();
+                while (resultsip1.next()) {
+                    String concacc = resultsip1.getString("uuid");
+                    altslist.add(concacc);
+                }
+                stmtip1.close();
+                resultsip1.close();
+                altslist.remove(uuid.toString().replace("-", ""));
+                if (!altslist.isEmpty()) {
+                    for (String alts : new ArrayList<>(altslist)) {
+                        if (!punishmngr.isBanned(alts)) {
+                            altslist.remove(alts);
+                        }
+                    }
+                    StringBuilder bannedalts = new StringBuilder();
+                    int i = 0;
+                    for (String alts : altslist) {
+                        bannedalts.append(NameFetcher.getName(alts));
+                        if (!(i + 1 >= altslist.size())) {
+                            bannedalts.append(", ");
+                        }
+                        i++;
+                    }
+                    if (bannedalts.length() > 0) {
+                        plugin.getProxy().getScheduler().schedule(plugin, () -> StaffChat.sendMessage(targetName + " Might have banned alts: " + bannedalts.toString()), 5, TimeUnit.SECONDS);
+                    }
+                }
+            } catch (SQLException sqle) {
+                try {
+                    throw new PunishmentsDatabaseException("Checking for banned alts", targetName, PlayerLogin.class.getName(), sqle);
+                } catch (PunishmentsDatabaseException pde) {
+                    ErrorHandler errorHandler = ErrorHandler.getInstance();
+                    errorHandler.log(pde);
+                    errorHandler.loginError(event);
+                }
+            }
+        });
     }
 }
