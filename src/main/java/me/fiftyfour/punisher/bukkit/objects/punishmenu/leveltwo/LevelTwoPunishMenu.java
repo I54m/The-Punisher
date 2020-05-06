214855df86ab0b500cdfba88840cdfbecb23fd20
54mpenguin <54mpenguin@gmail.com>
2020-05-06 16:34:59 +1200

    Added pre-loaded guis and made them multi version compatible

---
+++ src/main/java/me/fiftyfour/punisher/bukkit/objects/punishmenu/leveltwo/LevelTwoPunishMenu.java
@@ -0,0 +1,26 @@
+package me.fiftyfour.punisher.bukkit.objects.punishmenu.leveltwo;
+
+import me.fiftyfour.punisher.bukkit.PunisherBukkit;
+import me.fiftyfour.punisher.bukkit.objects.IconMenu;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+
+public interface LevelTwoPunishMenu {
+
+    IconMenu MENU = new IconMenu(4);
+
+    void setupMenu();
+
+    default void open(final Player punisher, String targetuuid, String targetName, String reputation) {
+        MENU.setClick((player, menu, slot, item) -> {
+            if (item.getItemMeta().getLore() != null) {
+                menu.close(player);
+                PunisherBukkit.getInstance().confirmationGUI.open(punisher, targetuuid, targetName, item, slot);
+                return true;
+            }
+            return false;
+        });
+        MENU.setName(ChatColor.RED + "Punish: " + targetName + " " + reputation);
+        MENU.open(punisher);
+    }
+}
